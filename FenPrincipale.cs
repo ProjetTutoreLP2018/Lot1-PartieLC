3332d57d73d9143db3287969a74fbc00163e7341
Jonathan <jonath.hamoudi@gmail.com>
2018-04-02 19:50:25 +0200

    Commit petites modifs

--- FenPrincipale.cs
+++ FenPrincipale.cs
@@ -1,17 +1,9 @@
 ﻿using ExcelDataReader;
 using Newtonsoft.Json;
 using System;
-using System.Collections;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
 using System.IO;
-using System.Linq;
-using System.Net;
 using System.Net.Http;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 using Xceed.Words.NET;
 
